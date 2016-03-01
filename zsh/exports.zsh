744afee13666be342a550ad8f6fe4e4fae3d5ba0
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-03-01 13:07:09 +0900

    export GOPATH

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -48,5 +48,6 @@ function() {
     typeset -gx EDITOR=$(filter 'has_a $@' $editors | head -n1)
     typeset -gx PAGER=$(filter 'has_a $@' $pagers | head -n1)
     typeset -gx CVS_RSH=ssh
+    typeset -gx GOPATH=~/data/src
 }
 
