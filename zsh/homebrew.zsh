df2d741b63a94d8194d7470b4ae57d83c9a75a3b
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-03-01 10:41:08 +0900

    command-not-found support

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -6,4 +6,5 @@ function() {
     mkdir -p $ZDOTDIR/functions
     [[ -h  $ZDOTDIR/functions/_brew ]] || ln -s $(brew --prefix)/Library/Contributions/brew_zsh_completion.zsh $ZDOTDIR/functions/_brew
     autoload -Uz _brew
+    brew command command-not-found-init > /dev/null && eval $(brew command-not-found-init)
 }
