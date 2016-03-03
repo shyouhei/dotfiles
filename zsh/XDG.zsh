5bf471f08a3476092cf974e263aafedfb054c964
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-03-03 18:01:49 +0900

    dont contaminate $HOME

--- zsh/XDG.zsh
+++ zsh/XDG.zsh
@@ -6,6 +6,8 @@
 typeset -gx PERLBREW_HOME=$XDG_CONFIG_HOME/perlbrew
 typeset -gx RBENV_ROOT=$XDG_CONFIG_HOME/rbenv
 typeset -gx INPUTRC=$XDG_CONFIG_HOME/readline/inputrc
+
 typeset -gx LESSHISTFILE=$XDG_CACHE_HOME/less/history
+typeset -gx GEM_SPEC_CACHE=$XDG_CACHE_HOME/rubygems/cache
 
 typeset -gx VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
