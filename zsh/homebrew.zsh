d1c3e69a6def237194df429ab960ffa1a1b07202
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-03-09 13:58:17 +0900

    set HOMEBREW_GITHUB_TOKEN

--- zsh/homebrew.zsh
+++ zsh/homebrew.zsh
@@ -7,4 +7,6 @@ function() {
     [[ -h  $ZDOTDIR/functions/_brew ]] || ln -s $(brew --prefix)/Library/Contributions/brew_zsh_completion.zsh $ZDOTDIR/functions/_brew
     autoload -Uz _brew
     brew command command-not-found-init > /dev/null && eval $(brew command-not-found-init)
+
+    typeset -x HOMEBREW_GITHUB_API_TOKEN=$(git config --get github.token)
 }
