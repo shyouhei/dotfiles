e8950b06598438a00d3985676aba6746044aed94
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-10-31 11:06:57 +0900

    follow file name change

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -4,7 +4,7 @@ typeset -x ZPLUG_HOME=$ZDOTDIR/zplug
 typeset -x ZPLUG_EXTERNAL=$ZDOTDIR/plugins.zsh
 typeset -x ZPLUG_USE_CACHE=true
 
-source $ZPLUG_HOME/zplug
+source $ZPLUG_HOME/init.zsh
 
 # zplug update --self
 zplug check || zplug install
