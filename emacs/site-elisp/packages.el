1d01779ef3020b2b644384376b037919dbad57b6
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2019-01-06 14:15:10 +0900

    use whitespace.el

--- emacs/site-elisp/packages.el
+++ emacs/site-elisp/packages.el
@@ -242,3 +242,28 @@
   :ensure t
   :config
   (editorconfig-mode 1))
+
+(package-install 'whitespace)
+(package-install 'unicode-whitespace)
+(use-package unicode-whitespace
+  :config (unicode-whitespace-setup))
+(use-package whitespace
+  :config
+  (progn
+    (add-hook 'ruby-mode-hook (lambda () (whitespace-mode 1)))
+    (add-hook 'c-mode-common-hook (lambda () (whitespace-mode 1)))
+    (setq whitespace-style '(face
+                             trailing
+                             tabs
+                             spaces
+                             lines-tail
+                             newline
+                             empty
+                             indentation
+                             space-after-tab
+                             space-before-tab
+                             space-mark
+                             tab-mark
+                             ))
+    (set-face-background 'whitespace-newline 'nil)
+    (set-face-background 'whitespace-space 'nil)))
