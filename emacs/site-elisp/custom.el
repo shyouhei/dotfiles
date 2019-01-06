8c8dda783a8d97664f98a102371b75e5f89a0ab9
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2019-01-06 17:22:20 +0900

    delete unused custom lines

--- emacs/site-elisp/custom.el
+++ emacs/site-elisp/custom.el
@@ -5,53 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cmake-font-lock cmake-mode magit editorconfig htmlize woman ansi-color tty-format company helm mic-paren w3m ggtags markdown-mode yaml-mode haml-mode slim-mode go-mode csharp-mode coffee-mode haskell-mode php-mode perl-mode electric-spacing ruby-electric ruby-block rbenv yard-mode ruby-mode exec-path-from-shell paradox diminish use-package)))
- '(safe-local-variable-values
-   (quote
-    ((eval add-hook
-	   (quote before-save-hook)
-	   (quote time-stamp))
-     (style . ruby)
-     (c-doc-comment-style . javadoc)
-     (change-log-indent-text . 2)
-     (add-log-time-format lambda
-			  (&optional x y)
-			  (let*
-			      ((time
-				(or x
-				    (current-time)))
-			       (system-time-locale "C")
-			       (diff
-				(+
-				 (cadr time)
-				 32400))
-			       (lo
-				(% diff 65536))
-			       (hi
-				(+
-				 (car time)
-				 (/ diff 65536))))
-			    (format-time-string "%a %b %e %H:%M:%S %Y"
-						(list hi lo)
-						t)))
-     (add-log-time-format lambda nil
-			  (let*
-			      ((time
-				(current-time))
-			       (system-time-locale "C")
-			       (diff
-				(+
-				 (cadr time)
-				 32400))
-			       (lo
-				(% diff 65536))
-			       (hi
-				(+
-				 (car time)
-				 (/ diff 65536))))
-			    (format-time-string "%a %b %e %H:%M:%S %Y"
-						(list hi lo)
-						t)))))))
+    (cmake-font-lock cmake-mode magit editorconfig htmlize woman ansi-color tty-format company helm mic-paren w3m ggtags markdown-mode yaml-mode haml-mode slim-mode go-mode csharp-mode coffee-mode haskell-mode php-mode perl-mode electric-spacing ruby-electric ruby-block rbenv yard-mode ruby-mode exec-path-from-shell paradox diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
