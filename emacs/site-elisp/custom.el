f4ab0d787cbeeb4d6653e23f33cf09b798be4e23
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2019-01-11 12:46:28 +0900

    custom.el updated

--- emacs/site-elisp/custom.el
+++ emacs/site-elisp/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cmake-font-lock cmake-mode magit editorconfig htmlize woman ansi-color tty-format company helm mic-paren w3m ggtags markdown-mode yaml-mode haml-mode slim-mode go-mode csharp-mode coffee-mode haskell-mode php-mode perl-mode electric-spacing ruby-electric ruby-block rbenv yard-mode ruby-mode exec-path-from-shell paradox diminish use-package))))
+    (unicode-whitespace whitespace editorconfig htmlize woman ansi-color company helm mic-paren w3m ggtags cmake-mode markdown-mode yaml-mode haml-mode slim-mode go-mode csharp-mode coffee-mode haskell-mode php-mode perl-mode electric-spacing ruby-electric ruby-block rbenv yard-mode ruby-mode exec-path-from-shell paradox diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
