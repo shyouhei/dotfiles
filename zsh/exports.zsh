ce4328ef5dec3810ce8e9c424b1d4d4f84b2bcec
卜部昌平 <shyouhei@st.inc>
2023-10-26 13:51:30 +0900

    change GOPATH

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -51,7 +51,7 @@ function() {
     typeset -gx EDITOR=$(filter 'has_a $@' $editors | head -n1)
     typeset -gx PAGER=$(filter 'has_a $@' $pagers | head -n1)
     typeset -gx CVS_RSH=ssh
-    typeset -gx GOPATH=~/data/src
+    typeset -gx GOPATH=~/data
     typeset -gx DOCKER_BUILDKIT=1
 }
 
